30b45adebc8f9d7d06bbc896fba9cfc0412b210f
Zach Ayers <ayers.dev@icloud.com>
2020-10-15 08:53:10 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EntityFramework.DatabaseFirst
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Get the DB Context
+            var context = new DatabaseFirstDemoEntities();
+
+            // Create a new post
+            var post = new Post {Body = "Body", DatePublished = DateTime.Now, Title = "Title", PostID = 1};
+
+            // Add the post to the context
+            context.Posts.Add(post);
+
+            // Save changes to the table
+            context.SaveChanges();
+        }
+    }
+}
